59dad596bfa7b3f74df2183e23dd4e90077432c7
Ababacar Asta <asta.hb2xy@gmail.com>
2025-06-11 21:39:39 +0000

    49 Tests

---
+++ src/test/java/com/example/doctorscarespringbootapplication/controller/admin/AdminMainControllerTest.java
@@ -0,0 +1,53 @@
+package com.example.doctorscarespringbootapplication.controller.admin;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminMainControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void adminHome() {
+    }
+
+    @Test
+    void patientProfile() {
+    }
+
+    @Test
+    void adminEmailingService() {
+    }
+
+    @Test
+    void emailServiceProcess() {
+    }
+
+    @Test
+    void patientTodayAppointment() {
+    }
+
+    @Test
+    void prescriptionLogs() {
+    }
+
+    @Test
+    void viewSinglePrescription() {
+    }
+
+    @Test
+    void patientsList() {
+    }
+
+    @Test
+    void doctorsList() {
+    }
+
+    @Test
+    void addCommonData() {
+    }
+}
